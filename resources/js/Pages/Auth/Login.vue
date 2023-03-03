4a0400330975f732c4a25b1c87104f60c937decc
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2023-03-03 11:51:42 +0100

    redesigned the login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -29,14 +29,9 @@ const submit = () => {
     <GuestLayout>
         <Head title="Log in" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
-
+                
                 <TextInput
                     id="email"
                     type="email"
@@ -45,6 +40,7 @@ const submit = () => {
                     required
                     autofocus
                     autocomplete="username"
+                     placeholder="Email"
                 />
 
                 <InputError class="mt-2" :message="form.errors.email" />
@@ -60,31 +56,33 @@ const submit = () => {
                     v-model="form.password"
                     required
                     autocomplete="current-password"
+                    placeholder="password"
                 />
 
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+            <div class="flex items-center justify-center pt-4">
+                 <PrimaryButton class="w-full" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
                 </PrimaryButton>
             </div>
+
+            <div class="flex items-center justify-center my-5">
+                <Link
+                    v-if="canResetPassword"
+                    :href="route('password.request')"
+                    class="hover:underline font-bold text-blue-500 focus:outline-none focus:ring-2 focus:ring-offset focus:ring-indigo-500"
+                >
+                    Forgotten password
+                </Link>
+            </div>
+               <div class="border-b border-b-gray-200"></div>
+                <div class="flex items-center justify-center pt-6 pb-2">
+                    <Link :href="route('register')" class="px-5 py-3 text-white bg-[#42B729] hover:bg-[#37A621] text-[20px] font-bold rounded-lg">
+                        Create New Account
+                    </Link>
+                </div>
         </form>
     </GuestLayout>
 </template>
